3455008d7d4fd1615110d1142ecd350602fa56ee
Ivan Garrido <ithsgarrido@gmail.com>
2020-12-15 00:14:03 +0100

    Problem med authentication. Kan inte logga in.

--- PresentationLayer/src/main/java/com/moose/presentationlayer/security/SpringSecurityConfig.java
+++ PresentationLayer/src/main/java/com/moose/presentationlayer/security/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -22,15 +23,29 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and().withUser("user").password("user").roles("USER");
 	}
 	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		
+		auth
+			.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("USER", "ADMIN");
+	}
+	
+	
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/css/**", "/webjars/**");
+	}
 	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("______now in configure.");
-		http.authorizeRequests().antMatchers("/", "/index/**", "/categoryBooks/**", "/productView/**", "/checkout", "/doCheckout", "/checkUser").permitAll()
+		http.authorizeRequests().antMatchers("/index/**", "/categoryBooks/**", "/productView/**", "/checkout", "/doCheckout", "/checkUser").authenticated()
 			
 			.and().authorizeRequests().antMatchers("/login", "/logout").permitAll()
-			.and().authorizeRequests().antMatchers("/static/css/**", "/js/**", "/images/**", "/**/favicon.ico").permitAll()
+			.and().authorizeRequests().antMatchers("/static/css/**", "/js/**", "/images/**", "/**/favicon.ico").authenticated()
+			.anyRequest().authenticated()
 			.and().formLogin().loginPage("/login").defaultSuccessUrl("/main").permitAll()
 			.and().logout()
 					.deleteCookies("remove")
